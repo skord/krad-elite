03e55b29b7924f284c45d4ce06bb143f1302eea4
Mike Danko <mike@l4m3.com>
2012-11-08 20:04:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kradelite::Application.config.secret_token = '1a782a52f5e1d3879afcaa6a729bc8f36a052976dca6aa16176adcf66abd4357a0901f04d79b193048dc50dbe61d32428d92635f82f34bcc5f5f1419c40310a6'
